8b0b7349da0a15c37cf14ee94d06d589ce663398
Ekali <23727482+ekaliclua@users.noreply.github.com>
2023-09-10 19:02:32 +0200

    Add hitman job

---
+++ lua/darkrp_modules/prisel_hitman/sh_prisel_hitman.lua
@@ -0,0 +1,26 @@
+TEAM_HITMAN = DarkRP.createJob( "Tueur à gages", {
+    color = Color( 238, 69, 69),
+    category = "Metiers illégaux",
+    model = {
+        "models/player/group01/male_01.mdl",
+        "models/player/group01/male_02.mdl",
+        "models/player/group01/male_03.mdl"
+    },
+    description = [[Le tueur à gages est un tueur professionnel qui est payé pour tuer des gens.]],
+    weapons = { "prisel_jumelles" },
+    command = "hitman_prisel",
+    max = 2,
+    salary = 0,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+} )
+
+-- DarkRP.createEntity( "Stand", {
+--     ent = "cardealer_stand",
+--     model = "models/stim/venatuss/car_dealer/stand.mdl",
+--     price = 1000,
+--     max = 5,
+--     cmd = "cardealer_stand",
+--     allowed = { TEAM_CARDEALER }
+-- })
